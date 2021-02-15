dcfe4282cee7d61b0498552e8ebaaf1577c73791
DavidLopezIBM <58993592+DavidLopezIBM@users.noreply.github.com>
2021-02-15 15:50:59 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 0.12"
   required_providers {
     ibm = {
-      source  = "ibm-cloud/ibm"
+      source  = "IBM-Cloud/ibm"
       version = "1.11.2"
     }
   }
